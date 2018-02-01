967b5b00881b908ff006a194d2829edbe0ce07ca
Florian Cousin <floriancousin@hotmail.com>
2018-02-01 16:14:17 +0100

    dry-custom projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,39 +1,34 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
-
   def show
-    @project = Project.find(params[:id])
   end
-
   def new
     @project = Project.new
   end
-
   def create
     Project.create(project_params)
     redirect_to projects_path
   end
-
   def edit
-     @project = Project.find(params[:id])
   end
-
   def update
-    @project = Project.find(params[:id])
     @project.update(project_params)
     redirect_to project_path
   end
-
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
     redirect_to projects_path
   end
 
   private
 
+  def set_project
+    @project = Project.find(params[:id])
+  end
   def project_params
     params.require(:project).permit(:name, :category, :cost)
   end
