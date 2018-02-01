bd7f0f9668eb58d36d96104f04c72ddc6b3ba1f4
Florian Cousin <floriancousin@hotmail.com>
2018-02-01 20:17:04 +0100

    add routes to programmes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ Rails.application.routes.draw do
 
   get 'projects', to: 'projects#index'
 
-  resources :projects
+  resources :projects do
+    resources :programmes
+  end
+
 
   resources :oases
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
