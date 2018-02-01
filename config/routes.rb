37729020c758569238672d99843e58f367b9ba31
Florian Cousin <floriancousin@hotmail.com>
2018-02-01 13:51:41 +0100

    add crud to project

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   get 'projects', to: 'projects#index'
 
+  resources :projects
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+projects = Project.create([{ name: "Une histoire de flute" }, { name: "Suites avec vue"}])
